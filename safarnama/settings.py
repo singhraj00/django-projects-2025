b2c770d6fb1b9025176a80fe784f90847fe8a4f3
Rahul Rathour <101623048+singhraj00@users.noreply.github.com>
2025-12-08 21:48:05 +0530

    Update settings.py

--- safarnama/settings.py
+++ safarnama/settings.py
@@ -193,8 +193,8 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 ## Email Configuration
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_BACKEND = os.environ.get('EMAIL_BACKEND', 'django.core.mail.backends.smtp.EmailBackend')
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_USER')  
