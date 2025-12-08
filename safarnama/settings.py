18ba775c89bcebffb4eff7c263175ab5e2b610a0
Rahul Rathour <101623048+singhraj00@users.noreply.github.com>
2025-12-08 16:34:57 +0530

    Update settings.py

--- safarnama/settings.py
+++ safarnama/settings.py
@@ -138,8 +138,8 @@ AUTH_USER_MODEL = 'user.User'
 
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv("DATABASE_URL"),
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
         conn_max_age=600,
         ssl_require=False
     )
